7ab265089fc136362b9a7eb22c9b8252401aa772
Felix Leitenberger <101555763+felixleitenberger@users.noreply.github.com>
2025-12-23 15:29:46 +0100

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Felix Leitenberger on 23.12.25.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
